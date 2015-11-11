3eec0b9d9dfdb1465e703d492332055b426608f8
Benjamin Pasero <benjpas@microsoft.com>
2015-11-11 11:52:09 +0100

    some first flow support and coloring

---
+++ src/flowMain.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+var vscode = require('vscode');
+
+function activate(context) {
+	console.log('Congratulations, your extension "vscode-flow" is now active!');
+}
+
+exports.activate = activate;
