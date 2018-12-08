3211665a00788cdf0baec7dbbbd62f0a57317fc7
Mayank Agarwal <mayank1791989@gmail.com>
2018-12-08 12:27:55 +0530

    chore(babel): update babel to v7

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// NOTE: using babel.config.js instead of .babelrc to force
+// babel to compile files inside lib/pkg/* using this config
+
+module.exports = function(api) {
+  api.env();
+
+  return {
+    plugins: ['@babel/plugin-proposal-class-properties'],
+
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: '6'
+          }
+        }
+      ],
+      '@babel/preset-flow'
+    ],
+
+    ignore: ['node_modules']
+  };
+};
