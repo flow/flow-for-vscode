43bd7b8b251ace3d22c258df33bb7930caf2dd15
Mayank Agarwal <mayank1791989@gmail.com>
2019-06-11 23:17:21 +0530

    chore(build): update node target to 10
    - min supported vscode version (1.30.0) already uses node v10 so no
      point of building plugin with 8.
    
    other changes
    - update babel

--- babel.config.js
+++ babel.config.js
@@ -14,7 +14,7 @@ module.exports = function(api) {
         '@babel/preset-env',
         {
           targets: {
-            node: '8',
+            node: '10',
           },
           modules: false,
         }
