e8367cd7319da6ff2b9286820636e65e82e941d4
Marshall Roch <mroch@fb.com>
2022-07-26 17:37:28 -0400

    upgrade rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import progress from 'rollup-plugin-progress';
 
@@ -20,20 +20,8 @@ const config = {
   },
   plugins: [
     nodeResolve(),
-    babel({ exclude: 'node_modules/**' }),
-    commonjs({
-      namedExports: {
-        // vscode-languageclient uses some dynamic __export function
-        // for export * from ''
-        'vscode-languageclient': [
-          'TextDocumentFeature',
-          'RevealOutputChannelOn',
-          'ErrorAction',
-          'CloseAction',
-          ...Object.keys(lsp),
-        ],
-      },
-    }),
+    babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
+    commonjs(),
     isProduction ? terser() : null,
     progress(),
   ],
